cd3b5ff77e4133a0db830055669192f2ee212ed4
Polina <pshibanova00@mail.ru>
2021-03-29 15:36:50 +0300

    feat: dream team is solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const result = members.filter(item => typeof item === 'string') .map(item => item.trim().slice(0, 1).toUpperCase()).sort().join('')
+  return result
 };
