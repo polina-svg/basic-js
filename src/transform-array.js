57f93ea2205bd4ab78b82cdc4346d129a3de2e87
Polina <pshibanova00@mail.ru>
2021-03-30 14:47:26 +0300

    feat: transform array is completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
-
 module.exports = function transform(arr) {
   try {Array.isArray(arr)} catch (err) {throw new Error (err)};
+  if(Array.isArray(arr) && arr.length === 0) return [];
+  const resultArray = []
+
+  for (let i = 0; i < arr.length; i++) {
+    switch(arr[i]) {
+      case '--discard-prev':
+        if(i - 1 >= 0 && arr[i - 2] !== '--discard-next') resultArray.pop();
+        break;
+      case '--double-prev':
+        if(i - 1 >= 0 && arr[i - 2] !== '--discard-next') resultArray.push(arr[i - 1]);
+        break;
+      case '--double-next':
+        if(i + 1 < arr.length) resultArray.push(arr[i + 1]);
+        break;
+      case '--discard-next':
+        if(i + 1 < arr.length) i++;
+        break;
+      default:
+        resultArray.push(arr[i]);
+    }
   
-};
+  }
+  return resultArray;
+}
